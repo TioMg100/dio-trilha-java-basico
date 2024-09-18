1d5bc1bce9e6a09cdec2b954a768ebd98712b2c3
chrystianm <chrystianmiguel@hotmail.com>
2024-09-18 14:43:38 -0400

    projeto3

---
+++ DesafioControleFluxo/src/main/java/org/dio/Contador.java
@@ -0,0 +1,44 @@
+package org.dio;
+
+import org.dio.excecao.ParametrosInvalidosException;
+
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = Integer.valueOf(scanner.next());
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = Integer.valueOf(scanner.next());
+
+        try {
+
+            contar(parametroUm, parametroDois);
+
+        } catch ( ParametrosInvalidosException e) {
+            System.out.println("O primeiro valor tem que ser menor que o segundo valor");
+        }
+
+
+    }
+
+    static void contar(int parametroUm, int parametroDois)   {
+
+        if (parametroUm < parametroDois){
+            int contagem = parametroDois - parametroUm;
+            for (int i = 1; i <=contagem-1 ; i++) {
+                System.out.printf("%d, ",i);
+
+            }
+                System.out.println(contagem);
+        } else {
+
+            throw new ParametrosInvalidosException();
+        }
+
+
+    }
+}
