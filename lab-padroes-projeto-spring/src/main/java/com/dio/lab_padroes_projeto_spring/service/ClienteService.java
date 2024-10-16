464c0dd0d66d0409d63b00967cff958cb5ad2c95
chrystianm <chrystianmiguel@hotmail.com>
2024-10-16 13:58:44 -0400

    lab

---
+++ lab-padroes-projeto-spring/src/main/java/com/dio/lab_padroes_projeto_spring/service/ClienteService.java
@@ -0,0 +1,18 @@
+package com.dio.lab_padroes_projeto_spring.service;
+
+
+import com.dio.lab_padroes_projeto_spring.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
---
+++ lab-padroes-projeto-spring/src/test/java/com/dio/lab_padroes_projeto_spring/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,14 @@
+package com.dio.lab_padroes_projeto_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+ class LabPadroesProjetoSpringApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
